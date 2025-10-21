ca1c3169b54fb1462708f2bbd65b75309a10c3a6
Nhật Tân <nhoxcon029@gmail.com>
2025-10-21 13:41:17 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-// const nextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,
-//   }
-// };
-
 const nextConfig = {
   output: 'export',
-  basePath: '/Lipice-AICG',
-  assetPrefix: '/Lipice-AICG/',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   }
 };
+
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/Lipice-AICG',
+//   assetPrefix: '/Lipice-AICG/',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   }
+// };
 
 
 module.exports = nextConfig;
