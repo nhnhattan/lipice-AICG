31489c8f8e26ea48b1d2cc0be5d3490068f4378a
Nhật Tân <nhoxcon029@gmail.com>
2025-10-21 13:26:44 +0700

    update

--- src/app/Gameplay/page.tsx
+++ src/app/Gameplay/page.tsx
@@ -2,13 +2,16 @@
 
 import Stepper from "@/components/Stepper";
 import Image from "next/image";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <>
       <div className="w-screen h-screen relative box-border flex items-center justify-center">
         <div className="w-full max-h-screen overflow-hidden py-2 flex flex-col items-center gap-4 relative box-border">
-          <Stepper />
+          <Suspense fallback={<div>Đang tải trò chơi...</div>}>
+            <Stepper />
+          </Suspense>
         </div>
       </div>
     </>
